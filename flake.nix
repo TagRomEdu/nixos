bcc96d83561f885a72cfe4320b62553d2761634b
Ly-sec <itslysec@gmail.com>
2025-04-25 14:53:38 +0200

    Restore working config from 499c1b3f and clean up

--- flake.nix
+++ flake.nix
@@ -17,12 +17,9 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    fabric.url = "github:Fabrics-Development/fabric";  # Add the Fabric flake
   };
 
-  outputs = { self, nixpkgs, home-manager, chaotic, fabric, ... }@inputs: {
-    # Define the NixOS configuration for your system
+  outputs = { self, nixpkgs, home-manager, chaotic, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit self inputs; };
@@ -34,32 +31,5 @@
         chaotic.nixosModules.default
       ];
     };
-
-    # Fabric overlay setup and package definitions for Python packages
-    nixpkgs.lib.eachDefaultSystem (system: let
-      overlay = final: prev: {
-        pythonPackagesExtensions =
-          prev.pythonPackagesExtensions ++ [
-            (python-final: python-prev: {
-              python-fabric = prev.callPackage fabric.default {};
-            })
-          ];
-      };
-
-      pkgs = nixpkgs.legacyPackages.${system}.extend overlay;
-    in {
-      overlays.default = overlay;
-
-      # Fabric's Python package and run-widget
-      packages = {
-        default = pkgs.python3Packages.python-fabric;
-        run-widget = pkgs.callPackage fabric.run-widget {};
-      };
-
-      # Reference the devShell from the dev-shells folder
-      devShells = {
-        default = import ./dev-shells/fabric-shell.nix { pkgs = pkgs; python3 = pkgs.python3; };
-      };
-    });
   };
 }
