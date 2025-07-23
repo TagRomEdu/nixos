3d11258c0e04c526506ff0152fe80a37249b4a93
TagRomEdu <qwertystyles@gmail.com>
2025-07-23 10:28:30 -0400

    now it works!

--- system/filesystems.nix
+++ system/filesystems.nix
@@ -2,7 +2,7 @@
 
 {
   fileSystems."/mnt/storage" = {
-    device = "UUID=74697b82-1266-4372-96cc-aac599abfb72";
+    device = "UUID=64898b80-15d1-4b7e-a6df-adcccf6c0e34";
     fsType = "ext4";
   };
 }
