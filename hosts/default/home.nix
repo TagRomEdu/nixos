83f4929ac803db5d4a1fc4d3ca7c84299a35474f
TagRomEdu <qwertystyles@gmail.com>
2025-07-27 16:44:44 +0500

    lasts big changes

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -50,4 +50,17 @@ in
   };
 
   programs.home-manager.enable = true;
+   
+  programs.carapace.enable = true;
+  programs.carapace.enableNushellIntegration = true;
+
+  programs.starship = { enable = true;
+      settings = {
+        add_newline = true;
+        character = { 
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+        };
+      };
+  };
 }
