d84f4b9bdffd01c88e9f4cfc3d2e4454de233598
TagRomEdu <qwertystyles@gmail.com>
2025-08-01 20:52:20 +0500

    added couple of themes to stylix

--- system/programs/stylix.nix
+++ system/programs/stylix.nix
@@ -2,11 +2,11 @@
 {
   stylix.enable = true;
   stylix.autoEnable = true;
-  #stylix.base16Scheme = toString (builtins.path {
-  #  path = ../../assets/themes/base-16/oled-lavender.yaml;
-  #});
+  stylix.base16Scheme = toString (builtins.path {
+    path = ../../assets/themes/base-16/oled-lavender.yaml;
+  });
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-light.yaml";
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/twilight.yaml";
+  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/twilight.yaml";
 
   stylix.enableReleaseChecks = false;
 }
