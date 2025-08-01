be57741d0aff055d22dc3377811dd14a32aba292
TagRomEdu <qwertystyles@gmail.com>
2025-08-01 14:52:39 +0500

    updated niri settings

--- home/niri/settings.nix
+++ home/niri/settings.nix
@@ -73,7 +73,7 @@
           };
           scale = 1.0;
           position = { x = 0; y = 0; };
-          focus-at-startup
+          focus-at-startup.enable = true;
         };
         
         "eDP-1" = {
