74c5718c1aefedc47da887dff467aa191f19f4a7
TagRomEdu <qwertystyles@gmail.com>
2025-08-01 13:41:43 +0500

    updated ghossty config

--- system/programs/stylix.nix
+++ system/programs/stylix.nix
@@ -2,10 +2,10 @@
 {
   stylix.enable = true;
   stylix.autoEnable = true;
-#  stylix.base16Scheme = toString (builtins.path {
-#    path = ../../assets/themes/base-16/oled-lavender.yaml;
-#  });
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon.yaml";
+  stylix.base16Scheme = toString (builtins.path {
+    path = ../../assets/themes/base-16/oled-lavender.yaml;
+  });
+  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon.yaml";
 
   stylix.enableReleaseChecks = false;
 }
