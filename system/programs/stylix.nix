453f5fce50340e217674d55f848beb7f5618748f
TagRomEdu <qwertystyles@gmail.com>
2025-08-05 09:50:34 +0500

    switch to light theme

--- system/programs/stylix.nix
+++ system/programs/stylix.nix
@@ -2,10 +2,10 @@
 {
   stylix.enable = true;
   stylix.autoEnable = true;
-  stylix.base16Scheme = toString (builtins.path {
-    path = ../../assets/themes/base-16/lavender.yaml;
-  });
-  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-light.yaml";
+#  stylix.base16Scheme = toString (builtins.path {
+#    path = ../../assets/themes/base-16/lavender.yaml;
+#  });
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-light.yaml";
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/twilight.yaml";
 
   stylix.enableReleaseChecks = false;
