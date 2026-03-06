424bdef6cc6152be4d7da141ecb0c5d36bc76c7f
TagRomEdu <qwertystyles@gmail.com>
2026-03-06 17:36:02 +0500

    chore: add widget for bt devices batter

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -36,10 +36,8 @@
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${pkgs.bash}/bin/bash /home/tre/nixos/home/scripts/bluetooth_batteries.sh";
-      Environment = ''
-        XDG_RUNTIME_DIR=/run/user/${config.users.users.tre.uid}
-        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${config.users.users.tre.uid}/bus
-      '';
+      Environment = "XDG_RUNTIME_DIR=/run/user/${config.users.users.tre.uid}";
+      Environment = "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${config.users.users.tre.uid}/bus";
     };
   };
 
